412b9577064c3f75e5064399a85304180605c53c
psadil <psadil@gmail.com>
2020-09-18 17:19:55 -0400

    construct rmarkdown

---
+++ data-raw/sub02.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+sub02 <- fst::read_fst(here::here("data-raw", "sub-02_task-con_area-V1_space-T1w_beta.fst")) %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(
+    sub = factor(sub),
+    voxel = factor(voxel),
+    run = factor(run),
+    ses = factor(ses))
+
+
+usethis::use_data(sub02, overwrite = TRUE)
