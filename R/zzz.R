be60abf905b64b4dc77121a36298011b39eec16d
psadil <psadil@gmail.com>
2020-09-28 15:41:09 -0400

    no more warnings about versions

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onLoad <- function(libname, pkgname){
-
-  if(!identical(Model$public_fields$cmdstanr_version, utils::packageVersion("cmdstanr")))
-    warning(
-      "The currently install CmdStanR version does not match
-      the version this package was build against. This can
-      cause subtle bugs! Please reinstall nmmr.")
-
-}
