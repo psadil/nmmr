da4e37a5f7cec850dc1c75567593691b9a08cef7
psadil <psadil@gmail.com>
2020-09-28 15:12:17 -0400

    added test for passing new data during fitting

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,5 @@
 .onLoad <- function(libname, pkgname){
 
-  if(!Model$public_fields$cmdstan_version == cmdstanr::cmdstan_version())
-    warning(
-      "The currently install CmdStan version does not match
-      the version this package was build against. This can
-      cause subtle bugs! Please reinstall nmmr.")
-
   if(!identical(Model$public_fields$cmdstanr_version, packageVersion("cmdstanr")))
     warning(
       "The currently install CmdStanR version does not match
--- tests/testthat/test-Model.R
+++ tests/testthat/test-Model.R
@@ -20,6 +20,23 @@ test_that("multiplicative model runs", {
   checkmate::expect_r6(f, classes = c("ModelMCMC"))
 })
 
+test_that("new data can be passed during sampling", {
+  {
+    sink("/dev/null")
+    suppressMessages(
+      f2 <- m$sample(
+        data = small,
+        iter_warmup = 5,
+        iter_sampling = 5,
+        chains = 2,
+        refresh = 0,
+        show_messages = FALSE))
+    sink()
+    }
+  checkmate::expect_r6(f2, classes = c("ModelMCMC"))
+  testthat::expect_identical(f$standata, f2$standata)
+})
+
 test_that("ModelFit object contains the data", {
   expect_identical(m$standata, f$standata)
 })
