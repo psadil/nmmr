bc13d09b447851f239cb4d3bd9736153cb084fb6
psadil <psadil@gmail.com>
2020-09-29 08:49:53 -0400

    ignore compiled models

--- R/utils.R
+++ R/utils.R
@@ -17,24 +17,3 @@ rad <- function(degree){
   (degree * pi) / 180
 }
 
-
-#' Convert between angles and radians
-#' @aliases rad deg
-#'
-#' @param radian,degree angles with different units
-#' @name angles
-NULL
-
-#' @rdname angles
-#' @export
-deg <- function(radian){
-  (radian * 180) / pi
-}
-
-#' @rdname angles
-#' @export
-rad <- function(degree){
-  (degree * pi) / 180
-}
-
-
