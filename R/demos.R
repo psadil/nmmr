9d7b6d0f39a1b007c6a4699176ce3490af7d10b0
Patrick Sadil <psadil@gmail.com>
2020-11-05 13:21:58 -0500

    ugh

--- R/demos.R
+++ R/demos.R
@@ -1,22 +1,22 @@
 
 
-make_d <- function(modulation,
-                   magnitude,
-                   x = seq(-pi, pi-2*pi/8, length.out = 8),
-                   kv = 2,
-                   center = -pi/16,
-                   p = 0.6) {
-  d <- tibble(raw = x) %>%
-    crossing(
+#' @param modulation subset of c("Additive", "Multiplicative", "Bandwidth", "Shift")
+#' @param x radians over which to cross
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_d0 <- function(modulation,
+                   x = seq(-pi, pi-2*pi/8, length.out = 8)) {
+
+  modulations <- c("Additive", "Multiplicative", "Bandwidth", "Shift")
+  checkmate::assert_subset(modulation, modulations, empty.ok = FALSE)
+
+  d <- tibble::tibble(raw = x) %>%
+    tidyr::crossing(
       Contrast = factor(c("Low", "High"), levels = c("Low", "High")),
-      VTF = factor(
-        c("Additive", "Multiplicative"),
-        levels = c("Additive", "Multiplicative"))) %>%
-    mutate(
-      y = case_when(
-        fct_match(Contrast, "Low") ~ CircStats::dmixedvm(x, center, center+pi, kv, kv, p),
-        fct_match(VTF, "Additive") ~ 0.1 + CircStats::dmixedvm(x, center, center+pi, kv, kv, p),
-        fct_match(VTF, "Multiplicative") ~ 1.4*CircStats::dmixedvm(x, center, center+pi, kv, kv, p)),
-      orientation = CircStats::deg(x / 2))
+      VTF = factor(modulation, levels = modulations))
+
   return(d)
 }
