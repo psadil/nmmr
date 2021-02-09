4d58d897efaa660d7ae29c3ad8741f8fd14afa3b
Patrick Sadil <psadil@gmail.com>
2021-02-09 10:56:20 -0500

    Close #42. The issue related to the location of inst when package installed

--- R/stanmodels.R
+++ R/stanmodels.R
@@ -4,7 +4,9 @@ stanmodels <- c("vtf", "deming")
 # instantiate each stanmodel object
 stanmodels <- sapply(stanmodels, function(model_name) {
   # create C++ code for stan model
-  stan_file <- file.path("inst", "stan", paste0(model_name, ".stan"))
+  # the two instances are for when the package is in development vs. installed
+  stan_file <- if(dir.exists("stan")) "stan" else file.path("inst", "stan")
+  stan_file <- file.path(stan_file, paste0(model_name, ".stan"))
 
   cmdstanr::cmdstan_model(stan_file, include_paths = file.path("inst", "stan"))
 })
